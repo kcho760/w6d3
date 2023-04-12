5edcc8b377bb6eb0e52dc7a31231ae478084d20e
AntonJames-Sistence <anton.james.ja@gmail.com>
2023-04-12 15:27:37 -0400

    phase 3

--- practice-for-ch-rails-first-routes-long-practice-main/first_routes/config/routes.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/first_routes/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   # resources :users
-    get 'users/:id', to: 'users#index', as: 'user'
+    get 'users', to: 'users#index', as: 'user'
     post 'users', to: 'users#create'
     get 'users/new', to: 'users#new', as: 'new_user'
     get 'users/:id/edit', to: 'users#edit', as: 'edit_user'
