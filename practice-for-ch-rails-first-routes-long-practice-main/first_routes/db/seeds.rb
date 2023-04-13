e93ca76cdcf001fe2f341ef3668edcd75e7ea847
kcho760 <kcho760@gmail.com>
2023-04-13 14:00:46 -0400

    added seed

--- practice-for-ch-rails-first-routes-long-practice-main/first_routes/db/seeds.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/first_routes/db/seeds.rb
@@ -5,3 +5,51 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ActiveRecord::Base.transaction do 
+    puts "Preparing environment"
+
+    puts 'Destroying tables'
+    User.destroy_all
+    Artwork.destroy_all
+    # Artwork_share.destroy_all
+
+    puts "Resetting id sequences"
+
+    # %w(users artworks artwork_shares).each do |table_name|
+    #     ApplicationRecord.connection.reset_pk_squence!(table_name)
+    # end
+
+    puts "creating seed data"
+
+    anton = User.create(
+        username: "Anton"
+    )
+    kevin = User.create(
+        username: "Kevin"
+    )
+    spencer = User.create(
+        username: "Spencer"
+    )
+    kin = User.create(
+        username: "Kin"
+    )
+
+    mona_lisa = Artwork.create(
+        title: "mona lisa",
+        image_url: "www.monalisa.com",
+        artist_id: kin.id
+    )
+
+    the_last_supper = Artwork.create(
+        title: "the last supper",
+        image_url: "www.thelastsupper.com",
+        artist_id: spencer.id
+    )
+    the_starry_night = Artwork.create(
+        title: "the starry night",
+        image_url: "www.thestarrynight.com",
+        artist_id: anton.id
+    )
+
+end
